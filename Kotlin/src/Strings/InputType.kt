efdfed480a57f4e499f26101ac24919718c523fc
Azamat <a_tletay@kbtu.kz>
2024-03-04 19:28:13 +0500

    Kotlin

---
+++ Kotlin/src/Strings/InputType.kt
@@ -0,0 +1,9 @@
+package Strings
+
+fun main(args: Array<String>) {
+    val first_name: String? = readLine()
+    val last_name: String? = readLine()
+
+    println("Hello , ${first_name} ${last_name}")
+}
+
